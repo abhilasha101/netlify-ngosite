ea1d123cbca80efdb705aaaa02fd7f3c24af1864
abhilasha <twinklemalhotra40@gmail.com>
2019-03-14 01:57:56 +0530

    netlify

---
+++ build/precache-manifest.2f9e753d163a592931cdf8b4c7543108.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f801d0c04ce6d8c1c6e6cd16df53409a",
+    "url": "/static/media/sc1.f801d0c0.jpg"
+  },
+  {
+    "revision": "27f1151595170947309840d284b30416",
+    "url": "/static/media/dog-wallpaper-14.27f11515.jpg"
+  },
+  {
+    "revision": "bea6dfe748c38f407529c2698ca8a3b6",
+    "url": "/static/media/d3.bea6dfe7.jpg"
+  },
+  {
+    "revision": "48c6f78ecee4f141ba1e2ab77cff5317",
+    "url": "/static/media/d2.48c6f78e.jpg"
+  },
+  {
+    "revision": "56dbf853e18f4e7703fbf343b2bcb666",
+    "url": "/static/media/d1.56dbf853.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bf1e745609fa6822f19b",
+    "url": "/static/js/main.bf1e7456.chunk.js"
+  },
+  {
+    "revision": "6c1f4402fbca9921d037",
+    "url": "/static/js/1.6c1f4402.chunk.js"
+  },
+  {
+    "revision": "bf1e745609fa6822f19b",
+    "url": "/static/css/main.8f19ecff.chunk.css"
+  },
+  {
+    "revision": "1568e800020320de14db572c65542ac8",
+    "url": "/index.html"
+  }
+];
